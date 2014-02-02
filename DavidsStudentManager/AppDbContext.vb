d90f9a156748a81d19b7488185897a27e14ef7b2
shawnweisfeld <shawn@shawnweisfeld.com>
2014-02-02 11:17:51 -0600

    save/load list from db

---
+++ DavidsStudentManager/AppDbContext.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.Entity
+
+Public Class AppDbContext
+    Inherits DbContext
+
+    Public Sub New()
+        MyBase.New("DefaultConnection")
+
+    End Sub
+
+    Private _students As DbSet(Of Student)
+    Public Property Students() As DbSet(Of Student)
+        Get
+            Return _students
+        End Get
+        Set(ByVal value As DbSet(Of Student))
+            _students = value
+        End Set
+    End Property
+
+
+End Class
